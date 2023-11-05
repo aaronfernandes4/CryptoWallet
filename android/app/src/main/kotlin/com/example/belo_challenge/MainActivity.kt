250a0e8656a51ed9fd86f69fd2335c865aa21318
premdhawane <premdhawane09@gmail.com>
2023-11-05 23:31:57 +0530

    Fetching data

--- android/app/src/main/kotlin/com/example/belo_challenge/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.belo_challenge
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- android/app/src/main/kotlin/com/example/yo/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.yo
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
